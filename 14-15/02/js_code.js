63b93b9ba22a3c2a3d580ba790120716bec41d0e
DmitryBormotov <drbormotov@gmail.com>
2025-11-23 02:32:25 +0300

    Update js_code.js

--- 14-15/02/js_code.js
+++ 14-15/02/js_code.js
@@ -1,12 +1,12 @@
 function find_edit(){
     const fname_node = document.getElementById('first_name')
-    fname_node.innerHTML="<b>DMITRY</b>"
+    fname_node.innerHTML="<b> DMITRY</b>"
     
     const sname_node = document.getElementById('second_name')
     sname_node.innerHTML="<b>BORMOTOV</b>"
     
     const dtb_node = document.getElementById('date_of_birthday')
-    dtb_node.innerHTML="<b>27.05.2007</b>"
+    dtb_node.innerHTML="<b> 27.05.2007</b>"
 }
 
 const node_for_click = document.getElementById("for_click")
